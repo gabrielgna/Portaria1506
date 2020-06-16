ad75a173aba869c40afd4b4eb490158aa72b16fb
unknown <gabriel.gna@hotmail.com>
2020-06-16 20:19:06 -0300

    listar Entradas

--- src/main/java/br/com/senai/fatesg/primefaces/controle/MovimentacaoControl.java
+++ src/main/java/br/com/senai/fatesg/primefaces/controle/MovimentacaoControl.java
@@ -1,12 +1,17 @@
 package br.com.senai.fatesg.primefaces.controle;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
+import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 
+import br.com.ambientinformatica.ambientjsf.util.UtilFaces;
 import br.com.senai.fatesg.primefaces.entidade.CadastradosEntity;
 import br.com.senai.fatesg.primefaces.entidade.Movimentacao;
 import br.com.senai.fatesg.primefaces.persistencia.MovimentacaoDao;
@@ -18,8 +23,25 @@ import org.springframework.context.annotation.Scope;
 public class MovimentacaoControl {
     private Movimentacao movimentacao = new Movimentacao();
 
+    private List<Movimentacao> listaMov = new ArrayList<Movimentacao>();
+
     @Autowired
     private MovimentacaoDao movimentacaoDao;
+
+    @PostConstruct
+    public void init() {
+        listar(null);
+    }
+
+
+    public void listar(ActionEvent evt) {
+        try {
+            listaMov = movimentacaoDao.listar();
+        } catch (Exception e) {
+            UtilFaces.addMensagemFaces(e);
+        }
+
+    }
 
 
     public void Confirmar(CadastradosEntity cadastradosEntity){
@@ -42,4 +64,11 @@ public class MovimentacaoControl {
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
+    public List<Movimentacao> getListaMov() {
+        return listaMov;
+    }
+
+    public void setListaMov(List<Movimentacao> listaMov) {
+        this.listaMov = listaMov;
+    }
 }
